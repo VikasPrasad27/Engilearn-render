97b5b2897f7f565c95ec01e3896ae75089264b1f
VikasPrasad27 <vjp66778899@gmail.com>
2025-04-06 20:36:34 +0530

    Update Login.jsx

--- client/engifrontend/src/components/Login.jsx
+++ client/engifrontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../store/auth";
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://engilearn.onrender.com";
 const Login = () => {
   const [user, setUser] = useState({
     name: "",
